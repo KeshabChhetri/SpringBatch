0abe2f7fedd87e3251c527abf81915297248f8db
whois <whois@DESKTOP-1RFS8AF.lan>
2020-07-01 22:33:50 -0500

    Pushing data from ExcelSheet to Database using SpringBatch

---
+++ SpringBatchExample/src/main/java/com/batch/model/Report.java
@@ -0,0 +1,31 @@
+package com.batch.model;
+
+public class Report {
+
+	private String Cid;
+	private String Cname;
+	private String Ccapital;
+	
+	public String getCid() {
+		return Cid;
+	}
+	public void setCid(String cid) {
+		Cid = cid;
+	}
+	public String getCname() {
+		return Cname;
+	}
+	public void setCname(String cname) {
+		Cname = cname;
+	}
+	public String getCcapital() {
+		return Ccapital;
+	}
+	public void setCcapital(String ccapital) {
+		Ccapital = ccapital;
+	}
+	
+	
+
+	
+}
